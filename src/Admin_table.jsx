011f49aa9e8525f487e1fa0805adf279174730b4
Aditya yadav <adityayadavadi6100@gmail.com>
2025-03-31 21:45:29 +0530

    new code

--- src/Admin_table.jsx
+++ src/Admin_table.jsx
@@ -130,8 +130,7 @@ const Admin = () => {
             />
             <button
               type="submit"
-              className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 transition"
-            >
+              className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 transition">
               Update
             </button>
           </form>
